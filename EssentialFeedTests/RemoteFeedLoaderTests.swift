af9b848bc23f44d938bbf25b2be564e34b1ba847
myh <curryhuang3627@gmail.com>
2022-12-01 15:37:03 +0800

    Replace deprecated Data init(bytes: ) with Data init()

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -66,7 +66,7 @@ class RemoteFeedLoaderTexts: XCTestCase {
         var capturedError = [RemoteFeedLoader.Error]()
         sut.load { capturedError.append($0) }
         
-        let invalidJSON = Data(bytes: "invalid json".utf8)
+        let invalidJSON = Data("invalid json".utf8)
         client.complete(withStatusCode: 200, data: invalidJSON)
         
         XCTAssertEqual(capturedError, [.invalidData])
