6cc9dc947130560ca2ce4a0fa18da0aeb87c181b
myh <curryhuang3627@gmail.com>
2022-12-05 09:36:07 +0800

    Creates URLSession data task with URL on 'URLSessionHTTPClient.get(from: URL)'

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -14,12 +14,12 @@ class URLSessionHTTPClient {
         self.session = session
     }
     func get(from url: URL) {
-        
+        session.dataTask(with: url) { _, _, _ in}
     }
 }
 class URLSessionHTTPClientTests: XCTestCase {
     
-    func test() {
+    func test_getFromURL_createsDataTaskWithURL() {
         let url = URL(string: "http://any-url.com")!
         let session = URLSessionSpy()
         let sut = URLSessionHTTPClient(session: session)
@@ -29,6 +29,7 @@ class URLSessionHTTPClientTests: XCTestCase {
         XCTAssertEqual(session.receivedURLs, [url])
     }
     
+    //MARK: - Helpers
     private class URLSessionSpy: URLSession {
         var receivedURLs = [URL]()
         
@@ -38,7 +39,6 @@ class URLSessionHTTPClientTests: XCTestCase {
         }
     }
     
-    private class FakeURLSessionDataTask: URLSessionDataTask {
-        
-    }
+    private class FakeURLSessionDataTask: URLSessionDataTask {}
+    
 }
