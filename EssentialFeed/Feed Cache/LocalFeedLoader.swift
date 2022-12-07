c67fc35cbf4c58c2834ba4742ff0922613c3f803
myh <curryhuang3627@gmail.com>
2022-12-07 16:59:36 +0800

    Add spacing

--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -39,11 +39,14 @@ public final class LocalFeedLoader {
             case let .failure(error):
                 self.store.deleteCacheFeed { _ in }
                 completion(.failure(error))
+                
             case let .found(feed, timestamp) where self.validate(timestamp):
                 completion(.success(feed.toModels()))
+                
             case .found:
                 self.store.deleteCacheFeed { _ in }
                 completion(.success([]))
+                
             case .empty:
                 completion(.success([]))
             }
