2a337896105b4fb0d4696145adacd0e46cd5c4e8
myh <curryhuang3627@gmail.com>
2022-12-07 16:46:42 +0800

    Remove untested test

--- EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -104,17 +104,6 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
         XCTAssertEqual(store.receivedMessage, [.retrieve])
     }
     
-//    func test_load_doesNotdeletesCacheOnSevenDaysOldCache() {
-//        let feed = uniqueImageFeed()
-//        let fixedCurrentDate = Date()
-//        let sevenDaysOldTimeStamp = fixedCurrentDate.adding(days: -7)
-//        let (sut, store) = makeSUT()
-//        
-//        sut.load { _ in }
-//        store.completeRetrieval(with: feed.local, timestamp: lessThanSevenDaysOldTimeStamp)
-//        
-//        XCTAssertEqual(store.receivedMessage, [.retrieve])
-//    }
     
     //MARK: - Helpers
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
