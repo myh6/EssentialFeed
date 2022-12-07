ca2289a8c51a6e77ca0c7be40f37f2c6d6692ada
myh <curryhuang3627@gmail.com>
2022-12-07 16:46:27 +0800

    Add current date to set

--- EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -96,13 +96,25 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
         let feed = uniqueImageFeed()
         let fixedCurrentDate = Date()
         let lessThanSevenDaysOldTimeStamp = fixedCurrentDate.adding(days: -7).adding(seconds: 1)
-        let (sut, store) = makeSUT()
+        let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
         
         sut.load { _ in }
         store.completeRetrieval(with: feed.local, timestamp: lessThanSevenDaysOldTimeStamp)
         
         XCTAssertEqual(store.receivedMessage, [.retrieve])
     }
+    
+//    func test_load_doesNotdeletesCacheOnSevenDaysOldCache() {
+//        let feed = uniqueImageFeed()
+//        let fixedCurrentDate = Date()
+//        let sevenDaysOldTimeStamp = fixedCurrentDate.adding(days: -7)
+//        let (sut, store) = makeSUT()
+//        
+//        sut.load { _ in }
+//        store.completeRetrieval(with: feed.local, timestamp: lessThanSevenDaysOldTimeStamp)
+//        
+//        XCTAssertEqual(store.receivedMessage, [.retrieve])
+//    }
     
     //MARK: - Helpers
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
