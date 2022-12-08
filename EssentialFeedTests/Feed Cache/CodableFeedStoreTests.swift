72adb9063ebeccc8fc96e0fcd6266e86d37ec892
myh <curryhuang3627@gmail.com>
2022-12-08 17:00:00 +0800

    Remove unfinished test

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -169,21 +169,6 @@ class CodableFeedStoreTests: XCTestCase {
         expect(sut, toRetrieve: .empty)
     }
     
-//    func test_delete_emptiesPreviouslyInsertedCache() {
-//        let sut = makeSUT()
-//        insert((uniqueImageFeed().local, Date()), to: sut)
-//
-//        let exp = expectation(description: "Wait for cache deletion")
-//
-//        sut.deleteCachedFeed { deletionError in
-//            XCTAssertNil(deletionError, "Expected empty cache deletion to succeed")
-//            exp.fulfill()
-//        }
-//        wait(for: [exp], timeout: 1.0)
-//
-//        expect(sut, toRetrieve: .empty)
-//    }
-    
     //MARK: - Helpers
     
     private func makeSUT(storeURL: URL? = nil, file: StaticString = #file, line: UInt = #line) -> CodableFeedStore {
