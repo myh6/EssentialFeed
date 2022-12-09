9f01eab781835de64992054bf1a70c09239fa43e
myh <curryhuang3627@gmail.com>
2022-12-09 17:16:00 +0800

    Add CoreData feed store specs

---
+++ EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -0,0 +1,45 @@
+//
+//  CoreDataFeedStoreTests.swift
+//  EssentialFeedTests
+//
+//  Created by Min-Yang Huang on 2022/12/9.
+//
+
+import XCTest
+import EssentialFeed
+
+class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
+    func test_retrieve_deliversEmptyOnEmptyCache() {
+        
+    }
+    
+    func test_retrieve_hasNoSideEffectOnEmptyCache() {
+        
+    }
+    
+    func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
+        
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
+        
+    }
+    
+    func test_retrieve_deliversFailureOnRetrievalError() {}
+    func test_retrieve_hasNoSideEffectsOnFailure() {}
+    
+    func test_insert_overridesPreiouslyInsertedCacheValues() {
+        
+    }
+    
+    func test_insert_deliversErrorOnInsertionError() {}
+    func test_insert_hasNoSideEffectOnInsertionError() {}
+    
+    func test_delete_deliversErrorOnDeletionError() {}
+    func test_delete_hasNoSideEffectOnDeletionError() {}
+    
+    func test_storeSideEffects_runSerially() {
+        
+    }
+    
+}
