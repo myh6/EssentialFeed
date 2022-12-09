55cccf0b108024da16941cb0160f6c8d2e56bbbc
myh <curryhuang3627@gmail.com>
2022-12-09 16:53:54 +0800

    Remove redundant func in ˋFeedStoreSpecsˋ protocol

--- EssentialFeedTests/Feed Cache/FeedStoreSpecs/FeedStoreSpecs.swift
+++ EssentialFeedTests/Feed Cache/FeedStoreSpecs/FeedStoreSpecs.swift
@@ -15,9 +15,6 @@ protocol FeedStoreSpecs {
     
     func test_insert_overridesPreiouslyInsertedCacheValues()
     
-    func test_delete_hasNoSideEffectsOnEmptyCache()
-    func test_delete_emptiesPreviouslyInsertedCache()
-    
     func test_storeeSideEffects_runSerially()
 }
 
