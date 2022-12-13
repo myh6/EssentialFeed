2b1768360afd2087bf6a82889988fc876d9102fc
myh <curryhuang3627@gmail.com>
2022-12-13 16:33:25 +0800

    Replace occurrences of ˋError?ˋ for representing operation success/failure with ˋResult<Void, Error>ˋ

--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -18,16 +18,16 @@ public final class LocalFeedLoader {
 }
 
 extension LocalFeedLoader {
-    public typealias SaveResult = Error?
+    public typealias SaveResult = Result<Void, Error>
     
     public func save(_ feed: [FeedImage], completion: @escaping (SaveResult) -> Void) {
-        store.deleteCacheFeed { [weak self] error in
+        store.deleteCacheFeed { [weak self] deletionResult in
             guard let self = self else { return }
-            
-            if let cachedDeletionError = error {
-                completion(cachedDeletionError)
-            } else {
+            switch deletionResult {
+            case .success:
                 self.cache(feed, with: completion)
+            case let .failure(error):
+                completion(.failure(error))
             }
         }
     }
