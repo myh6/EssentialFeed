013f4349705cf911dd7a7e5c289535db4fdf5223
myh <curryhuang3627@gmail.com>
2022-12-13 15:56:42 +0800

    Nest ˋResultˋ in ˋHTTPClientˋ

--- EssentialFeedTests/Feed API/LoadFeedFromRemoteUseCaseTests.swift
+++ EssentialFeedTests/Feed API/LoadFeedFromRemoteUseCaseTests.swift
@@ -168,13 +168,13 @@ class LoadFeedFromRemoteUseCaseTests: XCTestCase {
         /// 1. How many times the 'get' method was invoked (via the array count)
         /// 2. Which params were passed to the 'get' method invocations
         /// 3. Order of invocations (if necessary).
-        private var messages = [(url: URL, completion: (HTTPClientResult) -> Void)]()
+        private var messages = [(url: URL, completion: (HTTPClient.Result) -> Void)]()
         
         var requestedURLs: [URL] {
             return messages.map{ $0.url }
         }
         
-        func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
+        func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
             messages.append((url, completion))
         }
         
@@ -188,7 +188,7 @@ class LoadFeedFromRemoteUseCaseTests: XCTestCase {
                 statusCode: code,
                 httpVersion: nil,
                 headerFields: nil)!
-            messages[index].completion(.success(data, response))
+            messages[index].completion(.success((data, response)))
         }
         
     }
