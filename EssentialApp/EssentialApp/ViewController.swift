4c9985888a0283a0664a7f1f0e10ca9eebc07783
myh <curryhuang3627@gmail.com>
2023-01-15 22:40:00 +0800

    Add empty EssentialApp project

---
+++ EssentialApp/EssentialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialApp
+//
+//  Created by Min-Yang Huang on 2023/1/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
