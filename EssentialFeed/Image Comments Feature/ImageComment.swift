5a555bc013060a470df5f8542a2c5040c6a969b3
myh <curryhuang3627@gmail.com>
2023-01-30 16:46:22 +0800

    Add ImageComment data model

---
+++ EssentialFeed/Image Comments Feature/ImageComment.swift
@@ -0,0 +1,22 @@
+//
+//  ImageComment.swift
+//  EssentialFeed
+//
+//  Created by Min-Yang Huang on 2023/1/30.
+//
+
+import Foundation
+
+public struct ImageComment: Equatable {
+    public let id: UUID
+    public let message: String
+    public let createdAt: Date
+    public let username: String
+    
+    public init(id: UUID, message: String, createdAt: Date, username: String) {
+        self.id = id
+        self.message = message
+        self.createdAt = createdAt
+        self.username = username
+    }
+}
