afffdffb2eee7fc11a8008a5f763221853e738f3
myh <curryhuang3627@gmail.com>
2023-01-30 11:50:15 +0800

    Remove unnecessary property ˋOK_200ˋ in ˋFeedItemsMapperˋ

--- EssentialFeed/Feed API/FeedItemsMapper.swift
+++ EssentialFeed/Feed API/FeedItemsMapper.swift
@@ -13,8 +13,6 @@ internal final class FeedItemsMapper {
         let items: [RemoteFeedItem]
     }
     
-    private static var OK_200: Int { return 200 }
-    
     internal static func map(_ data: Data, from response: HTTPURLResponse) throws -> [RemoteFeedItem] {
         guard response.isOK, let root = try? JSONDecoder().decode(Root.self, from: data) else {
             throw RemoteFeedLoader.Error.invalidData
