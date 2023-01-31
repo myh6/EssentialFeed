a681513e36a521f91b53a708baaa675a69dcb3c4
myh <curryhuang3627@gmail.com>
2023-01-31 15:21:11 +0800

    Rename method

--- EssentialFeed/Shared Presentation/LoadResourcePresenter.swift
+++ EssentialFeed/Shared Presentation/LoadResourcePresenter.swift
@@ -18,7 +18,7 @@ public final class LoadResourcePresenter {
         self.feedView = feedView
     }
     
-    public func didStartLoadingFeed() {
+    public func didStartLoading() {
         errorView.display(.noError)
         loadingView.display(FeedLoadingViewModel(isLoading: true))
     }
