891f8d86173a1eda1fc03becbec3faaeab09a2b0
myh <curryhuang3627@gmail.com>
2023-02-01 15:28:12 +0800

    Map ImageComments into ImageCommentsViewModels

--- EssentialFeedTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
+++ EssentialFeedTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
@@ -23,17 +23,7 @@ extension Date {
         return adding(days: -feedCacheMaxAgeInDays)
     }
     
-    private func adding(days: Int) -> Date {
-        return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
-    }
-    
     private var feedCacheMaxAgeInDays: Int {
         return 7
-    }
-}
-
-extension Date {
-    func adding(seconds: TimeInterval) -> Date {
-        return self + seconds
     }
 }
