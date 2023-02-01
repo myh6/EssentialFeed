28894a38c0a735c7b0e231cdb4b764289859b53c
myh <curryhuang3627@gmail.com>
2023-02-01 10:28:13 +0800

    Remove unnecessary Localized class

--- EssentialFeed/Feed Presentation/FeedPresenter.swift
+++ EssentialFeed/Feed Presentation/FeedPresenter.swift
@@ -23,7 +23,10 @@ public final class FeedPresenter {
     private let errorView: FeedErrorView
     
     public static var title: String {
-        return Localized.Feed.title
+        NSLocalizedString("FEED_VIEW_TITLE",
+                          tableName: "Feed",
+                          bundle: Bundle(for: FeedPresenter.self),
+                          comment: "Title for the feed view")
     }
     private var feedLoadError: String {
         return NSLocalizedString("GENERIC_CONNECTION_ERROR", tableName: "Shared", bundle: Bundle(for: FeedPresenter.self), comment: "Error message displayed when we can't load the image feed from the server")
