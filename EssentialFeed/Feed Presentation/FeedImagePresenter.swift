8fc9b0dc44381038283b2901e1effd153fcea627
myh <curryhuang3627@gmail.com>
2023-02-01 13:57:21 +0800

    Remove unused InvalidImageDataError in FeedImagePresenter

--- EssentialFeed/Feed Presentation/FeedImagePresenter.swift
+++ EssentialFeed/Feed Presentation/FeedImagePresenter.swift
@@ -11,8 +11,6 @@ public protocol FeedImageView {
     func display(_ model: FeedImageViewModel<Image>)
 }
 
-private struct InvalidImageDataError: Error {}
-
 public final class FeedImagePresenter<View: FeedImageView, Image> where View.Image == Image {
     private let view: View
     private let imageTransformer: (Data) -> Image?
@@ -32,15 +30,13 @@ public final class FeedImagePresenter<View: FeedImageView, Image> where View.Ima
     }
     
     public func didFinishLoadingImageData(with data: Data, for model: FeedImage) {
-        guard let image = imageTransformer(data) else {
-            return didFinishLoadingImageData(with: InvalidImageDataError(), for: model)
-        }
+        let image = imageTransformer(data)
         view.display(FeedImageViewModel(
             description: model.description,
             location: model.location,
             image: image,
             isLoading: false,
-            shouldRetry: false))
+            shouldRetry: image == nil))
     }
     
     public func didFinishLoadingImageData(with error: Error, for model: FeedImage) {
