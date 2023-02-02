90ca404788877a9884e2d997ed679c0e764ef7f4
myh <curryhuang3627@gmail.com>
2023-02-02 09:48:32 +0800

    Rename FeedViewController to ListViewController since it now can render any collection of cell configuration

--- EssentialApp/EssentialAppTests/FeedAcceptanceTests.swift
+++ EssentialApp/EssentialAppTests/FeedAcceptanceTests.swift
@@ -58,13 +58,13 @@ class FeedAcceptanceTests: XCTestCase {
     //MARK: - Helpers
     
     private func launch(httpClient: HTTPClientStub = .offline,
-                        store: InMemoryFeedStore = .empty) -> FeedViewController {
+                        store: InMemoryFeedStore = .empty) -> ListViewController {
         let sut = SceneDelegate(httpClient: httpClient, store: store)
         sut.window = UIWindow()
         sut.configureWindow()
         
         let nav = sut.window?.rootViewController as? UINavigationController
-        let feed = nav?.topViewController as! FeedViewController
+        let feed = nav?.topViewController as! ListViewController
         return feed
     }
     
