a8c592e6e360e8cdf65402e63bf9d69a20e6ccab
myh <curryhuang3627@gmail.com>
2023-02-02 09:54:05 +0800

    Replace FeedViewControllerDelegate with a closure (Any protocol which only has one method can be replaced with a closure)

--- EssentialApp/EssentialApp/LoadResourcePresentationAdapter.swift
+++ EssentialApp/EssentialApp/LoadResourcePresentationAdapter.swift
@@ -34,12 +34,6 @@ final class LoadResourcePresentationAdapter<Resource, View: ResourceView> {
     }
 }
 
-extension LoadResourcePresentationAdapter: FeedViewControllerDelegate {
-    func didRequestFeedRefresh() {
-        loadResource()
-    }
-}
-
 extension LoadResourcePresentationAdapter: FeedImageCellControlDelegate {
     func didRequestImage() {
         loadResource()
