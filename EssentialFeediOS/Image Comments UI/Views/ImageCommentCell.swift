71e68dc09e5b440e62553b4e60cd532ecf83ea7f
myh <curryhuang3627@gmail.com>
2023-02-04 11:00:18 +0800

    Remove unused property

--- EssentialFeediOS/Image Comments UI/Views/ImageCommentCell.swift
+++ EssentialFeediOS/Image Comments UI/Views/ImageCommentCell.swift
@@ -12,10 +12,4 @@ public final class ImageCommentCell: UITableViewCell {
     @IBOutlet private(set) public var messageLabel: UILabel!
     @IBOutlet private(set) public var usernameLabel: UILabel!
     @IBOutlet private(set) public var dateLabel: UILabel!
-    
-    var onRetry: (() -> Void)?
-    
-    @IBAction private func retryButtonTapped() {
-        onRetry?()
-    }
 }
