7c0a4a3d48370a86a911b05ea8c116e9f6098188
myh <curryhuang3627@gmail.com>
2023-02-06 17:12:49 +0800

    Set FeedEndpoint limit to 10

--- EssentialFeed/Feed API/FeedEndpoint.swift
+++ EssentialFeed/Feed API/FeedEndpoint.swift
@@ -14,12 +14,14 @@ public enum FeedEndpoint {
     public func url(baseURL: URL) -> URL {
         switch self {
         case .get:
-            if #available(macOS 13.0, *) {
-                return baseURL.appending(path: "/v1/feed")
-            } else {
-                // Fallback on earlier versions
-                return baseURL.appendingPathComponent("/v1/feed")
-            }
+            var components = URLComponents()
+            components.scheme = baseURL.scheme
+            components.host = baseURL.host
+            components.path = baseURL.path + "/v1/feed"
+            components.queryItems = [
+                URLQueryItem(name: "limit", value: "10")
+            ]
+            return components.url!
         }
     }
 }
