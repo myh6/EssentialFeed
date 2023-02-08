fbe78edea8b7e21252ca55b920e146b43f76b0bd
myh <curryhuang3627@gmail.com>
2023-02-08 11:38:58 +0800

    Add default presentation mapper when the Resource type match the ResourceViewModel type

--- EssentialApp/EssentialApp/FeedViewAdapter.swift
+++ EssentialApp/EssentialApp/FeedViewAdapter.swift
@@ -78,8 +78,7 @@ final class FeedViewAdapter: ResourceView {
                 imageLoader: imageLoader,
                 selection: selection),
             loadingView: WeakRefVirtualProxy(loadMore),
-            errorView: WeakRefVirtualProxy(loadMore),
-            mapper: { $0 })
+            errorView: WeakRefVirtualProxy(loadMore))
 
         let loadMoreSection = [CellController(id: UUID(), loadMore)]
         
