aab8e41438a6e4b2e6289e58e61e300513511cb0
myh <curryhuang3627@gmail.com>
2023-02-08 14:45:10 +0800

    Load images synchronously

--- EssentialFeedTests/Feed Cache/Helpers/FeedImageDataStoreSpy.swift
+++ EssentialFeedTests/Feed Cache/Helpers/FeedImageDataStoreSpy.swift
@@ -14,7 +14,7 @@ class FeedImageDataStoreSpy: FeedImageDataStore {
         case insert(data: Data, for: URL)
     }
     
-    private var retrievalCompletions = [(FeedImageDataStore.RetrievalResult) -> Void]()
+    private var retrievalResult: Result<Data?, Error>?
     private var insertionResult: Result<Void, Error>?
     private(set) var receivedMessage = [Message]()
     
@@ -23,9 +23,9 @@ class FeedImageDataStoreSpy: FeedImageDataStore {
         try insertionResult?.get()
     }
     
-    func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
+    func retrieve(dataForURL url: URL) throws -> Data? {
         receivedMessage.append(.retrieve(dataFor: url))
-        retrievalCompletions.append(completion)
+        return try retrievalResult?.get()
     }
     
     func completeInsertion(with error: Error, at index: Int = 0) {
@@ -37,10 +37,10 @@ class FeedImageDataStoreSpy: FeedImageDataStore {
     }
     
     func completeRetrieval(with error: Error, at index: Int = 0) {
-        retrievalCompletions[index](.failure(error))
+        retrievalResult = .failure(error)
     }
     
     func completeRetrieval(with data: Data?, at index: Int = 0) {
-        retrievalCompletions[index](.success(data))
+        retrievalResult = .success(data)
     }
 }
