bf741ac9e7bb5bce792c5ad53f92ac843febf101
myh <curryhuang3627@gmail.com>
2023-02-08 15:57:09 +0800

    Remove unused async methods

--- EssentialFeed/Feed Cache/FeedImageDataStore.swift
+++ EssentialFeed/Feed Cache/FeedImageDataStore.swift
@@ -6,9 +6,6 @@
 //
 
 public protocol FeedImageDataStore {
-    typealias RetrievalResult = Swift.Result<Data?, Error>
-    typealias InsertionResult = Swift.Result<Void, Error>
-    
     func insert(_ data: Data, for url: URL) throws
     func retrieve(dataForURL url: URL) throws -> Data?
     
@@ -16,37 +13,6 @@ public protocol FeedImageDataStore {
     // func retrieve(dataForURL url: URL) -> AnyPubisher<Data?, Error>
     
     // completionHandler asynchronous
-    @available(*, deprecated)
-    func insert(_ data: Data, for url: URL, completion: @escaping (InsertionResult) -> Void)
-    @available(*, deprecated)
-    func retrieve(dataForURL url: URL, completion: @escaping (RetrievalResult) -> Void)
-}
-
-public extension FeedImageDataStore {
-    func insert(_ data: Data, for url: URL) throws {
-        let group = DispatchGroup()
-        group.enter()
-        var result: InsertionResult!
-        insert(data, for: url) {
-            result = $0
-            group.leave()
-        }
-        group.wait()
-        return try result.get()
-    }
-    
-    func retrieve(dataForURL url: URL) throws -> Data? {
-        let group = DispatchGroup()
-        group.enter()
-        var result: RetrievalResult!
-        retrieve(dataForURL: url, completion: {
-            result = $0
-            group.leave()
-        })
-        group.wait()
-        return try result.get()
-    }
-    
-    func insert(_ data: Data, for url: URL, completion: @escaping (InsertionResult) -> Void) {}
-    func retrieve(dataForURL url: URL, completion: @escaping (RetrievalResult) -> Void) {}
+    // @available(*, deprecated)
+    // func retrieve(dataForURL url: URL, completion: @escaping (RetrievalResult) -> Void)
 }
